6f4a231f8788364e6de56f331e9ba26b73da16d5
aisikaodepeople <761595535@qq.com>
2023-07-12 13:42:51 +0800

    Update RedisLockUtil.java
    redis锁

--- src/main/java/com/unionpay/demo/util/RedisLockUtil.java
+++ src/main/java/com/unionpay/demo/util/RedisLockUtil.java
@@ -18,18 +18,6 @@ public class RedisLockUtil {
     }
 
     /**
-     * 加锁
-     *
-     * @param lockKey
-     * @return
-     */
-    public static RLock lock(String lockKey) {
-        RLock lock = redissonClient.getLock(lockKey);
-        lock.lock();
-        return lock;
-    }
-
-    /**
      * 释放锁
      * <p>多线程环境下会失效报错<p/>
      *
@@ -38,19 +26,6 @@ public class RedisLockUtil {
     public static void unlock(String lockKey) {
         RLock lock = redissonClient.getLock(lockKey);
         lock.unlock();
-    }
-
-    /**
-     * 带超时的锁
-     *
-     * @param lockKey
-     * @param timeout 毫秒
-     * @return
-     */
-    public static RLock lock(String lockKey, int timeout) {
-        RLock lock = redissonClient.getLock(lockKey);
-        lock.lock(timeout, TimeUnit.MILLISECONDS);
-        return lock;
     }
 
     /**
@@ -69,16 +44,6 @@ public class RedisLockUtil {
             log.error("尝试加锁失败:", e);
             return false;
         }
-    }
-
-    /**
-     * 默认加锁 等待3秒，10秒失效
-     *
-     * @param lockKey
-     * @return
-     */
-    public static boolean defaultTryLock(String lockKey) {
-        return tryLock(lockKey, 3000, 10000);
     }
 
 }
